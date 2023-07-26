9b4ae16984ac35839fd0e00a57b7bc5abb5f1c80
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-07-26 14:51:58 +0200

    update DB

---
+++ DB/homework2/homework2.sql
@@ -0,0 +1,43 @@
+В рамках БД "онлайн-магазин" напишите след/запросы:
+
+Вывести название и стоимость в USD одного самого дорогого проданного товара
+
+SELECT
+    ProductName,
+    Price * 1.1 AS Price_usd
+FROM [Products]
+ORDER BY Price DESC
+    LIMIT 1
+
+Вывести два самых дорогих товара из категории Beverages из USA
+
+SELECT
+    *
+FROM [Products]
+    JOIN Categories ON Products.CategoryID=Categories.CategoryID
+    JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+WHERE Categories.CategoryName='Beverages'
+  AND
+    Suppliers.Country='USA'
+ORDER BY Price DESC
+    LIMIT 2
+
+Удалить товары с ценой менее 5 EUR
+
+DELETE FROM Products
+WHERE Price <= 5
+
+Вывести список стран, которые поставляют морепродукты
+
+SELECT DISTINCT
+    Suppliers.Country
+FROM Products
+         JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+         JOIN Categories ON Products.CategoryID=Categories.CategoryID
+WHERE CategoryName='Beverages'
+
+Очистить поле ContactName у всех клиентов не из China
+
+UPDATE Customers
+SET ContactName=''
+WHERE NOT Country = 'China'
